7f3c9c4dd49b226effef6688a61279596f8d4d1e
kunan01 <kunan01@163.com>
2019-03-20 11:38:25 +0800

    提交代码 删除 @EnableAutoConfiguration

--- emall/emall/src/main/java/com/tangmo/emall/EmallApplication.java
+++ emall/emall/src/main/java/com/tangmo/emall/EmallApplication.java
@@ -2,13 +2,9 @@ package com.tangmo.emall;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
-@EnableAutoConfiguration
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("com.tangmo.emall.dao")
